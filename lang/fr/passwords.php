e5dc5a8fc0278b841c9a7e7899024b7562220490
Julien <julien@julien-debian.home>
2024-04-04 10:30:43 +0200

    Traductions

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Mot de passe réinitialisé.',
+    'sent' => 'Un lien de réinitialisation de mot de passe vient de t\'être envoyé.',
+    'throttled' => 'Merci de patienter un peu avant de réessayer.',
+    'token' => 'Token de réinitialisation invalide.',
+    'user' => "Aucun utilisateur connu avec cette adresse e-mail.",
+
+];
