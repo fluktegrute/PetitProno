c79c432804d637d61ee924bb7f82c5ed5fe39b2f
Julien <julien@julien-debian.home>
2024-04-18 09:02:43 +0200

    Add lien vers le formulaire d'inscription depuis la page de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,13 @@
                 {{ __('Connexion') }}
             </x-primary-button>
         </div>
+        <div class="flex items-center space-between mt-4">
+            @if (Route::has('register'))
+                <span class="text-sm mr-2">Pas encore de compte ?</span>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    Inscris-toi !
+                </a>
+            @endif
+        </div>
     </form>
 </x-guest-layout>
